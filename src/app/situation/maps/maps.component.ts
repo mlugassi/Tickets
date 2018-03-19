bfafc941446983537c491af76223f93e9c660422
rzanzuri <refaelz1@walla.co.il>
2018-03-19 23:28:51 +0200

    בניית העמוד של המסמך החדש

--- src/app/situation/maps/maps.component.ts
+++ src/app/situation/maps/maps.component.ts
@@ -10,7 +10,7 @@ import { stat } from 'fs';
 })
 export class MapsComponent {
 
-  title: string = 'My first AGM project';
+  title: string = 'AGM project';
   lat: number = 51.678418;
   lng: number = 7.809007;
   location = "Israel";
