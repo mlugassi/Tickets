c5696761f782cb7276ed94e24efb40c678b08595
rzanzuri <refaelz1@walla.co.il>
2018-03-19 21:49:29 +0200

    Google maps Works!!

--- src/app/situation/maps/maps.component.ts
+++ src/app/situation/maps/maps.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Response } from '@angular/http';
 import { MapsService } from './maps.service';
+import { stat } from 'fs';
 
 @Component({
   selector: 'app-maps',
@@ -13,18 +14,34 @@ export class MapsComponent {
   lat: number = 51.678418;
   lng: number = 7.809007;
   location = "Israel";
-  maps= [];
-  constructor(private mapsService: MapsService){}
+  constructor(private mapsService: MapsService) { }
 
   searchLocation() {
+    //this.location=
     this.mapsService.getLatAndLong(this.location)
       .subscribe(
         (response: Response) => {
-           let data  = response.text(); //arrayBuffer();// json().results.geometry.location.lat;
-           alert(data);
-           //this.cars = data;
-       },
-       (error)    => console.log(error)
-    );
+          let data = response.text();
+          let status_begin = data.search("status");
+          let status = '"' + data.substring(status_begin, data.length-2);
+          if (status.includes("OK")) {
+            let location_begin = data.search("location");
+            let sub_data = data.substring(location_begin, data.length);
+            let lat_begin = sub_data.search("lat");
+            let lng_begin = sub_data.search("lng");
+            let lat = Number(sub_data.substring(lat_begin + 6, lat_begin + 16));
+            let lng = Number(sub_data.substring(lng_begin + 6, lng_begin + 16));
+            if (isNaN(lat) || isNaN(lng))
+              alert("Error with parsing " + lat + " and " + lng);
+            else {
+              this.lat = lat;
+              this.lng = lng;
+            }
+          }
+          else
+          alert("The Status isn't OK!! \n" + status);
+        },
+        (error) => console.log(error)
+      );
   }
 }
